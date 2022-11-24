565d1ed0c03b2cf3b5c16e35254d91bf4209093a
NishiReina <s1f101901195@iniad.org>
2022-11-24 12:18:42 +0900

    initial commit

---
+++ app/src/main/java/com/example/executor/MainActivity.java
@@ -0,0 +1,101 @@
+package com.example.executor;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.Locale;
+
+public class MainActivity extends AppCompatActivity {
+    private Handler handler = new Handler();
+    private Button button;
+    private TextView textView;
+//    private String urlIpText = "http://httpbin.org/ip";
+    private String urlIpText = "https://attendanceble.onrender.com/api/passcodeInfo/";
+    private String ip = "";
+    private String nameAndType = "";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        button = findViewById(R.id.button);
+        textView = findViewById(R.id.textView);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Thread thread = new Thread(new Runnable() {
+                    @Override
+                    public void run() {
+                        String response = "";
+                        try {
+                            response = getAPI();
+                            JSONObject rootJSON = new JSONObject(response);
+                            ip = rootJSON.getString("passcode");
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                        handler.post(new Runnable() {
+                            @Override
+                            public void run() {
+                                textView.setText(ip);
+                            }
+                        });
+                    }
+                });
+                thread.start();
+            }
+        });
+    }
+
+    public String getAPI(){
+        HttpURLConnection urlConnection = null;
+        InputStream inputStream = null;
+        String result = "";
+        String str = "";
+        try {
+            URL url = new URL(urlIpText);
+            urlConnection = (HttpURLConnection) url.openConnection();
+            urlConnection.setConnectTimeout(10000);
+            urlConnection.setReadTimeout(10000);
+            urlConnection.addRequestProperty("User-Agent", "Android");
+            urlConnection.addRequestProperty("Accept-Language", Locale.getDefault().toString());
+            urlConnection.setRequestMethod("GET");
+            urlConnection.setDoInput(true);
+            urlConnection.setDoOutput(false);
+            urlConnection.connect();
+            int statusCode = urlConnection.getResponseCode();
+            if (statusCode == 200){
+                inputStream = urlConnection.getInputStream();
+                BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream, "utf-8"));
+                result = bufferedReader.readLine();
+                while (result != null){
+                    str += result;
+                    result = bufferedReader.readLine();
+                }
+                bufferedReader.close();
+            }
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return str;
+    }
+}
